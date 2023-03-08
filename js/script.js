87531826d44b9299c3b0a66dc61e9e2ea40da74c
cioby09 <claudiociobanu09@gmail.com>
2023-03-08 15:50:37 +0100

    Add: project scaffolding

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// IMPUT
+// Chiedo all'utente il numero di chilometri che vuole percorrere
+
+// Chiedo all'utente l'età del passeggero
+
+// LOGICA
+// Calcolo il prezzo base del viaggio
+
+// Se il passeggero è minorenne
+//      Applico uno sconto del 20%
+// Altrimenti SE il passeggero è over 65
+//      Applico uno sconto del 40%
+
+// OUTPUT
+// Stampo il messaggio
