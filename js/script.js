19b7c24194c416d999692a845c58c856efffe97e
cioby09 <claudiociobanu09@gmail.com>
2023-03-08 16:10:28 +0100

    Add: price program

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
 // IMPUT
-// Chiedo all'utente il numero di chilometri che vuole percorrere
-
-// Chiedo all'utente l'età del passeggero
+// Chiedo all'utente il numero di chilometri che vuole percorrere e l'età del passeggero
+const km = prompt("Quanti chilometri vuoi percorrere?");
+const age = prompt("Quanti anni hai?")
+console.log(km, age);
 
 // LOGICA
 // Calcolo il prezzo base del viaggio
+const basePrice = km * 0.21;
+console.log(basePrice);
+
+// Calcolo gli sconti
+const discount20 = basePrice * 0.2;
+const discount40 = basePrice * 0.4;
+console.log(discount20, discount40);
 
 // Se il passeggero è minorenne
 //      Applico uno sconto del 20%
-// Altrimenti SE il passeggero è over 65
+// Altrimenti se il passeggero è over 65
 //      Applico uno sconto del 40%
+// Altrimenti
+//      Il prezzo di base rimane invariato
+let price ="";
+if(age < 18) {
+    price = basePrice - discount20;
+} else if (age > 65) {
+    price = basePrice - discount40;
+} else {
+    price = basePrice;
+}
 
 // OUTPUT
 // Stampo il messaggio
+console.log(price);
+document.getElementById("price").innerHTML = price;
